79cdffb59f8a033675b508290b3a7402e5ffe5ea
Sergio Astudillo <sergioastudillo@protonmail.com>
2021-04-12 21:03:19 +0200

    Make the input invisible once POSTed for better UI and UX

--- src/components/newsletter/index.js
+++ src/components/newsletter/index.js
@@ -21,7 +21,7 @@ class NewsletterForm extends React.Component {
 		¡Muchas gracias!`;
 		const userNotValidatedMessage = `Su email ya ha sido registrado, pero todavía no ha verificado su identidad.
 		Por favor, revise el correo electrónico proporcionado.`;
-		const firstRegister = `Revise el email de verificación enviado a la siguiente dirección de correo electrónico: ${this.state.email}.
+		const firstRegister = `Revise el email de verificación enviado a la siguiente dirección de correo electrónico: ${this.state.email}
 		Puede seguir navegando por la web si lo desea.`;
 
 		let elementExists = this.state.newsletter.find(user => user.email === this.state.email);
@@ -55,10 +55,14 @@ class NewsletterForm extends React.Component {
 	render() {
 		const { email, message } = this.state;
 		let styleFirstParagraph = '';
+		let styleTitle = 'my-2 underline uppercase';
+		let styleInput = 'rounded text-center w-3/4 px-3 border border-gray-600';
 		let styleButton = 'my-2 h-6 max-h-10 bg-custom-darkEnf text-white tex uppercase px-3 rounded-3xl border border-gray-600';
 		let styleLink = 'hidden';
 		if (message !== '') {
 			styleFirstParagraph = 'mt-2 mx-3 whitespace-pre-line';
+			styleTitle += ' hidden';
+			styleInput += ' hidden';
 			styleButton += ' hidden';
 			styleLink = 'my-2 py-1 text-center bg-custom-darkEnf text-white uppercase px-3 rounded-3xl border border-gray-600 inline-block';
 		} else {
@@ -69,9 +73,8 @@ class NewsletterForm extends React.Component {
 			<section className='w-screen h-1/2 font-josefin mt-5'>
 				<form className='bg-gray-200 w-3/4 mx-auto border-2 border-gray-600 rounded-xl' onSubmit={this.handleSubmit}>
 					<p className={styleFirstParagraph}>{message}</p>
-					<p className='my-2 underline uppercase'>Email:</p>
-					<input className='rounded text-center w-3/4 px-3 border border-gray-600' type='email' required placeholder='email@email.com' name='email' value={email} onChange={this.handleInputChange} />
-					<br />
+					<p className={styleTitle}>Email:</p>
+					<input className={styleInput} type='email' required placeholder='email@email.com' name='email' value={email} onChange={this.handleInputChange} />
 					<Link to='/' className={styleLink}>
 						Regresar al inicio
 					</Link>
