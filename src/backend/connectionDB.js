409dd03c17840dd23cdbc6dba9d242654d609eb4
Sergio Astudillo <sergioastudillo@protonmail.com>
2021-04-20 00:31:33 +0200

    Small correction

--- src/backend/connectionDB.js
+++ src/backend/connectionDB.js
@@ -5,12 +5,13 @@ function connect() {
 	mongoose
 		.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 		.then(() => {
-			console.log('Conectado a la BD satisfactoriamente.');
+			console.log('Connected to DB.');
 		})
 		.catch(err => console.log(err));
 }
 function close() {
 	mongoose.connection.close();
+	console.log('Connection closed.');
 }
 
 module.exports = {
